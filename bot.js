0dd8aaea333996f9d92f37b268ea9fd5e7640e5c
NoHaxJustEki <60154351+NoHaxJustEki@users.noreply.github.com>
2020-07-26 22:18:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+	wait(1000);
+	client.guilds.cache.forEach(g => {
+		g.fetchInvites().then(guildInvites => {
+			invites[g.id] = guildInvites;
+		});
+	});
+	
+});
 
 
 client.on('ready', () =>{
@@ -17,15 +29,22 @@ client.on('guildMemberAdd', member =>{
     const guild = client.guilds.cache.get('735645868413288510')
     const role = guild.roles.cache.get('735649648727031908')
     const channel = guild.channels.cache.get('735649072991567922')
-    const embed = new Discord.MessageEmbed()
-        .setColor('#115aed')
-        .setTitle('**Welcome to Coomunity Discord Server**')
-        .setDescription(`Dobrodosli ${member2} na nas server.\n\nInvajtovan od strane **${Inviter}**\nNadamo se da ce te uzivati.\n\nMi smo zajednica koja se trudi da ostvari komunikaciju izmedju ljudi, kao i da vam doprinese odlicnu zabavu.\nPre nego sto krenete molimo vas procitajte **PRAVILA**`)
-        .setTimestamp(new Date())
-    member.user.send(embed)
-    channel.send(embed);
-    member.roles.add(role)
+    member.guild.fetchInvites().then(guildInvites => {
+		const ei = invites[member.guild.id];
+		invites[member.guild.id] = guildInvites;
+		const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+		const inviter = client.users.cache.get(invite.inviter.id);
+		const embed = new Discord.MessageEmbed()
+            .setColor('#115aed')
+            .setTitle('**Welcome to Coomunity Discord Server**')
+            .setDescription(`Dobrodosli ${member2} na nas server.\nNadamo se da ce te uzivati.\n\nMi smo zajednica koja se trudi da ostvari komunikaciju izmedju ljudi, kao i da vam doprinese odlicnu zabavu.\nPre nego sto krenete molimo vas procitajte **PRAVILA**\n\nInvajtovan od strane **${inviter.tag}**\nInvite Link: https://discord.gg/invite/${invite.code}`)
+            .setTimestamp(new Date())
+        channel.send(embed);
+        member.roles.add(role)
+        member.user.send(embed)
+    });
 });
+ 
 
 client.on('guildMemberRemove', member =>{
     const member2 = member.user.username
@@ -137,12 +156,14 @@ client.on('message', msg => {
             }
             else {
                 const user = msg.mentions.users.first()
-                const embed = new Discord.MessageEmbed()
-                .setColor(`#115aed`)
-                .setTitle('**User Check**')
-                .setDescription(`Account: ${user.username}\nCreated at: ${user.createdAt}`)
-                .setTimestamp(new Date())
-                msg.channel.send(embed)
+                    const embed = new Discord.MessageEmbed()
+                    .setColor(`#115aed`)
+                    .setTitle('**User Check**')
+                   - .setDescription(`Account: ${user.username}\nCreated at: ${user.createdAt}`)
+                    .setTimestamp(new Date())
+                    msg.channel.send(embed)
+                    
+                
             }
             
         }
